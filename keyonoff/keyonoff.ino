fc8b71f002c92b35175ef9487775263786ab308a
James <james.groves@ncas.ac.uk>
2015-09-07 12:10:35 +0100

    Case statements added
    0 stop motor
    1 ccw motor direction
    3 cw motor direction

--- keyonoff/keyonoff.ino
+++ keyonoff/keyonoff.ino
@@ -11,14 +11,6 @@ void setup() {
   Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
   delay(1000);
   Serial.println("Starting motor control script");
-
-  Serial.println("resetting rotator to 0 degrees");
-  digitalWrite(led, HIGH); // turn on led while the rotator moves
-  digitalWrite(12, HIGH); //Establishes forward direction of Channel A
-  digitalWrite(9, LOW);   //Disengage the Brake for Channel A
-  analogWrite(3, 255);   //Spins the motor on Channel A at full speed
-  delay(14000);
-  digitalWrite(led, LOW); // turn off led when the rotator stops
   Serial.println("Motor is now under your control");
   Serial.println("Please select 1 for cw and 3 for ccw and 0 to stop all movement");
  
@@ -28,28 +20,29 @@ void loop(){
 
 
 
-             
-                Serial.println("Turning motor to 90 degrees ");
-                digitalWrite(12, LOW); //Establishes forward direction of Channel A
+                if (Serial.available() > 0) {
+                int inByte = Serial.read();
+                
+                switch (inByte) {
+                case '1':    
+                Serial.println("Starting motor");
+                digitalWrite(12, HIGH); //Establishes forward direction of Channel A
                 digitalWrite(9, LOW);   //Disengage the Brake for Channel A
                 analogWrite(3, 255);   //Spins the motor on Channel A at full speed
-                digitalWrite(led, HIGH); // turn on led while the rotator moves
-                delay(34000);
-                
+                break;
+        
+                case '0':    
                 Serial.println("Stopping motor");
-                digitalWrite(9, HIGH);  //Engage the Brake for Channel A 
-                digitalWrite(led, LOW); // turn off led when the rotator stops
-                delay(30000000);
-           
+                digitalWrite(9, HIGH);  //Engage the Brake for Channel A          
+                break;
+
+                case '3':    
+                Serial.println("Motor backwards");
+                digitalWrite(12, LOW);  //Establishes backward direction of Channel A 
+                digitalWrite(9, LOW);   //Disengage the Brake for Channel A         
+                analogWrite(3, 255);    //Spins the motor on Channel A at half speed
+                break;
+                                
+                }
+        }
 }
-  
-
-
-
-
-
-
-
-
-
-
