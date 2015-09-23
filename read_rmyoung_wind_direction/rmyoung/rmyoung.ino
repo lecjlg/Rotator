bf3cbd527cc2e866bcbb0731bbe7fb6bdda01438
James <james.groves@ncas.ac.uk>
2015-09-23 10:39:49 +0100

    directory name change

--- read_rmyoung_wind_direction/rmyoung/rmyoung.ino
+++ read_rmyoung_wind_direction/rmyoung/rmyoung.ino
@@ -9,8 +9,7 @@ void loop() {
                   int rmValue = analogRead(A3);
                   float rmvoltage = rmValue * (5.03 / 1023.0);
                   rmvoltage = rmvoltage * 72;
-                  //rmvoltage = rmvoltage * 11.69767441860465;
-                  //rmvoltage = rmvoltage / 0.0138888888888889; 
+                  
   Serial.println(rmvoltage);
   
 
